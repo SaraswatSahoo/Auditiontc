75c9781efcd74ce54b07a2f7dfb0dd4348c6153b
Sayan <sayanbanerjee2002@gmail.com>
2024-01-22 16:50:12 +0530

    removed login from navbar and added favicon of MNTC

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,49 +1,24 @@
-import { signIn, signOut, useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 
 export default function Navbar() {
   const { data: session, status } = useSession();
-  if (status === "unauthenticated" || status === "loading") {
-    return (
-      <>
-      <div className='h-20 w-full bg-gradient-to-b from-[#17212e] to-100% '>
-      <div className='absolute left-10 mt-4 '>
-          <Image src='/mntc.png' alt='logo'  width={120} height={60} />
-        </div>
-        <div className='mt-4 text-white  absolute right-10 '>
-
-    
-        <button
-          className=' bg-slate-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-10'
-          onClick={() => {
-            signIn("google", { callbackUrl: "/" });
-          }}
-        >
-          Login
-        </button>
-        </div>
-       
-      </div>
-    </>
-    )
-  }
+  const authButtonClass =
+    "bg-slate-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-10";
   return (
     <>
       <div className='h-20 w-full bg-gradient-to-b from-[#17212e] to-100% '>
-      <div className='absolute left-10 mt-4 '>
-          <Image src='/mntc.png' alt='logo'  width={120} height={60} />
+        <div className='absolute left-10 mt-4 '>
+          <Image src='/mntc.png' alt='logo' width={120} height={60} />
         </div>
         <div className='mt-4 text-white  absolute right-10 '>
-
-        {session?.user.email}
-        <button
-          className=' bg-slate-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-10'
-          onClick={() => signOut()}
-        >
-          Signout
-        </button>
+          {session?.user.email}
+          {status === "authenticated" && (
+            <button className={authButtonClass} onClick={() => signOut()}>
+              Signout
+            </button>
+          )}
         </div>
-       
       </div>
     </>
   );
