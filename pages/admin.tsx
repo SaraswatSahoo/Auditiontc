4a41c9a5223aa7a18b8567cf2a92e767412e3e58
Sayan <sayanbanerjee2002@gmail.com>
2024-01-23 23:45:08 +0530

    fix/admin account not accessable

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -10,7 +10,7 @@ import { IUser } from "../types/types";
 export default function admin({ users }: { users: IUser[] }) {
   return (
     <>
-      <div className='text-white overflow-auto max-h-screen'>
+      <div className='text-white overflow-auto max-h-screen pt-28'>
         <h1 className='text-2xl mb-10'>Total Registrations:{users.length}</h1>
         <div className='flex flex-col gap-10 '>
           {users.map((user, index) => (
@@ -35,7 +35,7 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
     return redirect("/", req.url!);
 
   await dbConnect();
-  const users = await User.find({ role: { $ne: "admin", $exists: true } });
+  const users = await User.find({ role1: { $ne: "admin", $exists: true } });
   return {
     props: {
       users: JSON.parse(JSON.stringify(users)),
